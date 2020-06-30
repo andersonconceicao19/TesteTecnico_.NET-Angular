3b11ae20279313fcd79442fca17786f494071024
Anderson Conceicao <andersonconceiicao@gmail.com>
2020-06-30 16:49:08 -0300

    Definindo estrutuda da entidade, Contexto de Dados, Mappings. DLLs prontas para migrations

---
+++ BackEnd/src/TesteTecnico.Data/Context/MeuDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteTecnico.Domain.Models;
+
+namespace TesteTecnico.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options)
+        { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+
+            modelBuilder.Entity<Usuario>()
+                .Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BackEnd/src/TesteTecnico.Data/Mappings/UsuarioMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TesteTecnico.Domain.Models;
+
+namespace TesteTecnico.Data.Mapping
+{
+    public class UsuarioMapping : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(pk => pk.Id);                                
+            builder.Property(u => u.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+            builder.Property(u=> u.Email)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+        }
+    }
+}
