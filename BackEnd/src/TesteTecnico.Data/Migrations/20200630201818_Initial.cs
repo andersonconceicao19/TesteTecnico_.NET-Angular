c90133dbfd28773adaa64b1db78622183c945134
Anderson Conceicao <andersonconceiicao@gmail.com>
2020-06-30 22:33:50 -0300

    Iniciando FrontEnd

---
+++ BackEnd/src/TesteTecnico.Data/Migrations/20200630201818_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TesteTecnico.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Sobrenome = table.Column<string>(type: "50", nullable: false),
+                    Email = table.Column<string>(type: "varchar(50)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(nullable: false),
+                    TipoEscolaridade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
--- BackEnd/src/TesteTecnico.Data/Migrations/20200630202324_Initial.Designer.cs
+++ BackEnd/src/TesteTecnico.Data/Migrations/20200630202324_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TesteTecnico.Data.Context;
 
 namespace TesteTecnico.Data.Migrations
 {
     [DbContext(typeof(MeuDbContext))]
-    partial class MeuDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200630202324_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -39,7 +41,7 @@ namespace TesteTecnico.Data.Migrations
 
                     b.Property<string>("Sobrenome")
                         .IsRequired()
-                        .HasColumnType("50");
+                        .HasColumnType("varchar50");
 
                     b.Property<int>("TipoEscolaridade")
                         .HasColumnType("int");
