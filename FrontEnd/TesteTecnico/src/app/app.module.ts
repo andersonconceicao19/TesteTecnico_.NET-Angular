3eb470a9e83f268e4d7ec8732726c0c20a42a08d
Anderson Conceicao <andersonconceiicao@gmail.com>
2020-06-30 23:16:38 -0300

    montando o header e footer  da aplicação

--- FrontEnd/TesteTecnico/src/app/app.module.ts
+++ FrontEnd/TesteTecnico/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CriarUsuarioComponent } from './pages/home/criar-usuario/criar-usuario.component';
+import { AtualizarUsuarioComponent } from './pages/home/atualizar-usuario/atualizar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    CriarUsuarioComponent,
+    AtualizarUsuarioComponent
   ],
   imports: [
     BrowserModule,
